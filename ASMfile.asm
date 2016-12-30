f738d11396a24385931313a3ea364cca6b0791ca
Natsumi <retrogreensnake@gmail.com>
2016-12-30 14:25:36 +0200

    + MusicTool can now insert banks * Fixed documentation * Fixed build SMPS ASM file.bat

--- ASMfile.asm
+++ ASMfile.asm
@@ -1,10 +1,9 @@
+Maincode	section org(0)
 	include "exe/macro.asm"
 	include "exe/smps2asm.asm"
-Maincode	section org(0)
 	include "asmfile equ.asm"
 	include	"\drv\/smps2asm equ.asm"; include smps2asm macros
 
-	selectdrv \drv
-	incmusasmonly	\fl
-
+	opt ae-
+	include	"music/\fl\.asm"
 	END
