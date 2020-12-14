69ed92fffb4b68e4e908008347efd3a7e8a98f20
Natsumi <retrogreensnake@gmail.com>
2020-12-14 16:13:55 +0200

    misc updates

--- ASMfile.asm
+++ ASMfile.asm
@@ -1,8 +1,7 @@
 Maincode	section org(0)
 	include "code/macro.asm"
-	include "code/smps2asm.asm"
 	include "asmfile equ.asm"
-	include	"smps/\drv\/smps2asm.asm"; include smps2asm macros
+	include	"smps/\drv\/smps2asm (ASM68K).asm"; include smps2asm macros
 
 	opt ae-
 	include	"music/\fl\.asm"
